91ef0ffd82c0d34cc88e0880c2b18d270490a144
Penagwin <langpenguin@gmail.com>
2014-09-20 14:13:24 +0000

    Remembering the selected location

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
 // Docs at http://simpleweatherjs.com
+function createCookie(name,value) {
+  var days = 120;
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	document.cookie = name+"="+value+expires+"; path=/";
+}
 
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return "Jenison, Michigan";
+}
+
+function eraseCookie(name) {
+	createCookie(name,"",-1);
+}
 $(document).ready(function() {
-    var place = "Jenison, Michigan";
-    jQuery.ajax( { 
-      url: '//freegeoip.net/json/', 
-      type: 'POST', 
-      dataType: 'jsonp',
-      success: function(location) {
-       setPlace(location.city+", " +location.region_name);
-       getWeather();
-       setWeather();
+    var place = readCookie("place");
+    if(place === "Jenison, Michigan"){
+        jQuery.ajax( { 
+          url: '//freegeoip.net/json/', 
+          type: 'POST', 
+          dataType: 'jsonp',
+          success: function(location) {
+           setPlace(location.city+", " +location.region_name);
+           getWeather();
+           setWeather();
+        }
+          
+        });
     }
-      
-    });
     console.log(place);
     function setPlace(nplace){
       place = nplace;
@@ -29,7 +52,7 @@ $(document).ready(function() {
           }
           html += '<div id="place">'+weathercity+', '+weatherregion+'</div>';
           html += '    <input id="place_entry" style="display:none;"></input>';
-
+          createCookie("place", place);
       
           $("#weather").html(html);
           $('#place').click(function() {
